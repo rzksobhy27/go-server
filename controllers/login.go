f5d0e2676902c1f9851acdeef8a3dd03a96b6330
Sobhy Rzk <rzksobhy27@gmail.com>
2022-11-13 02:14:35 +0200

    implementing JWT

--- controllers/login.go
+++ controllers/login.go
@@ -2,9 +2,13 @@ package controllers
 
 import (
 	"errors"
+	"fmt"
+	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/binding"
+	jwt "github.com/golang-jwt/jwt/v4"
 	"github.com/rzksobhy27/go-server/inits"
 	"github.com/rzksobhy27/go-server/tables"
 	"gorm.io/gorm"
@@ -13,7 +17,7 @@ import (
 func UserLogin(c *gin.Context) {
 	body := tables.User{}
 	if err := c.ShouldBindBodyWith(&body, binding.JSON); err != nil {
-		c.Status(200)
+		c.Status(http.StatusOK)
 		return
 	}
 
@@ -21,21 +25,33 @@ func UserLogin(c *gin.Context) {
 	result := inits.DB.Where("name = ?", body.Name).Select("password").First(&user)
 
 	if errors.Is(result.Error, gorm.ErrRecordNotFound) {
-		c.JSON(200, gin.H{
+		c.JSON(http.StatusOK, gin.H{
 			"err": "username not found",
 		})
 		return
 	}
 
 	if body.Password != user.Password {
-		c.JSON(200, gin.H{
+		c.JSON(http.StatusOK, gin.H{
 			"err": "passwords doesn't match!",
 		})
 		return
 	}
 
-	c.JSON(200, gin.H{
-		"msg":  "logged in successfully",
-		"user": body,
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"user":     body.Name,
+		"password": body.Password,
 	})
+
+	key := os.Getenv("TOKEN_KEY")
+	tokenString, err := token.SignedString([]byte(key))
+	if err != nil {
+		fmt.Printf("err: %v\n", err)
+		c.Status(http.StatusOK)
+		return
+	}
+
+	c.SetCookie("token", tokenString, 30*60*1000, "/", "localhost", false, false)
+
+	c.Status(http.StatusOK)
 }
--- controllers/register.go
+++ controllers/register.go
@@ -2,9 +2,13 @@ package controllers
 
 import (
 	"errors"
+	"fmt"
+	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/binding"
+	"github.com/golang-jwt/jwt/v4"
 	"github.com/rzksobhy27/go-server/inits"
 	"github.com/rzksobhy27/go-server/tables"
 	"gorm.io/gorm"
@@ -13,14 +17,14 @@ import (
 func UserRegister(c *gin.Context) {
 	body := tables.User{}
 	if err := c.ShouldBindBodyWith(&body, binding.JSON); err != nil {
-		c.Status(200)
+		c.Status(http.StatusOK)
 		return
 	}
 
 	result := inits.DB.Where("name = ?", body.Name).Select("").First(&tables.User{})
 
 	if !errors.Is(result.Error, gorm.ErrRecordNotFound) {
-		c.JSON(200, gin.H{
+		c.JSON(http.StatusOK, gin.H{
 			"err": "username already exists!",
 		})
 		return
@@ -28,9 +32,20 @@ func UserRegister(c *gin.Context) {
 
 	inits.DB.Create(body)
 
-	c.JSON(200, gin.H{
-		"msg": "user created successfully",
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"user":     body.Name,
+		"password": body.Password,
 	})
 
-	c.Status(200)
+	key := os.Getenv("TOKEN_KEY")
+	tokenString, err := token.SignedString([]byte(key))
+	if err != nil {
+		fmt.Printf("err: %v\n", err)
+		c.Status(http.StatusOK)
+		return
+	}
+
+	c.SetCookie("token", tokenString, 30*60*1000, "/", "localhost", false, false)
+
+	c.Status(http.StatusOK)
 }
